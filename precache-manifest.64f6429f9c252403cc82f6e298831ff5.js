e4844b0ce39898bc5b5b6779e25b05cdd46d7755
Paul M Edwards <pauledwards@gmail.com>
2019-10-16 21:18:18 -0700

    Updates

---
+++ precache-manifest.64f6429f9c252403cc82f6e298831ff5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0671986d8d40c7a7dd65721f1975b10e",
+    "url": "/Front-End/index.html"
+  },
+  {
+    "revision": "0053c9b11a5911c40268",
+    "url": "/Front-End/static/css/main.68b9f934.chunk.css"
+  },
+  {
+    "revision": "cd0b64cd30e2b0aa7116",
+    "url": "/Front-End/static/js/2.def7ef2a.chunk.js"
+  },
+  {
+    "revision": "0053c9b11a5911c40268",
+    "url": "/Front-End/static/js/main.0e876075.chunk.js"
+  },
+  {
+    "revision": "75e7fcb74a80c7552288",
+    "url": "/Front-End/static/js/runtime-main.591d3c31.js"
+  }
+]);
