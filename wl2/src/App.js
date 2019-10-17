db9e0f3abbe5721f3eb98de335085aed67d0dd68
patrick-replogle <patrickr1138@gmail.com>
2019-10-17 16:06:12 -0700

    initial commit and added a basic form and axios call.

--- wl2/src/App.js
+++ wl2/src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Route } from "react-router-dom";
 import {
-  WelcomePage
-  , Register
-  , Signin
-  , Profile
-  , Main
+  WelcomePage,
+  Register,
+  Signin,
+  Profile,
+  Main,
   // , Item
   // , List
-  , ListItem
+  ListItem,
   // , Task
-  , TaskItem
+  TaskItem
   // , Dialog
   // , Reminder
   // , Search
-} from './Components';
+} from "./Components";
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={WelcomePage}/>
-      <Route path="/register" component={Register}/>
-      <Route path="/signin" component={Signin}/>
-      <Route path="/profile" component={Profile}/>
-      <Route path="/my" component={Main}/>
-      <Route path="/my/list/:listid" component={ListItem}/>
-      <Route path="/my/list/:listid/task/:taskid" component={TaskItem}/>
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/register" component={Register} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/my" component={Main} />
+      <Route path="/my/list/:listid" component={ListItem} />
+      <Route path="/my/list/:listid/task/:taskid" component={TaskItem} />
     </div>
   );
 }
--- wl2/src/Components/CreateList.js
+++ wl2/src/Components/CreateList.js
@@ -1,11 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const CreateList = () => {
-    return(
-        <div>
-            <h4>CreateList</h4>
-        </div>
-    )
-}
+const CreateList = props => {
+  const [list, setList] = useState([{ title: "", task: "", setDate: "" }]);
+
+  const addList = note => {
+    const newList = {
+      id: Date.now(),
+      title: list.title,
+      task: list.task,
+      setDate: list.setDate
+    };
+
+    props.setLists([newList, ...props.lists]);
+  };
+
+  const handleChange = event => {
+    setList({ ...list, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    // backend url
+    // const url = "https://wunderlist-2.herokuapp.com/api/todos/";
+    event.preventDefault();
+    const newList = {
+      id: list.length,
+      title: list.title,
+      task: list.task,
+      setDate: list.setDate
+    };
+    axios({
+      method: "post",
+      url: "https://reqres.in/api/users/",
+      headers: {
+        Authorization: localStorage.getItem("token")
+      },
+      data: newList
+    })
+      .then(response => {
+        addList(response.data);
+      })
+      .catch(error => {
+        console.log(error, "error");
+      });
+    setList({ title: "", task: "", setDate: "" });
+  };
+
+  return (
+    <div className="form-page-container">
+      <h1>new list</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={list.title}
+          onChange={handleChange}
+        />
+        <label htmlFor="task">Task</label>
+        <input
+          type="text"
+          id="task"
+          name="task"
+          value={list.task}
+          onChange={handleChange}
+        />
+        <label htmlFor="date">Due Date</label>
+        <input
+          type="date"
+          id="setDate"
+          name="setDate"
+          placeholder="Due Date"
+          value={list.setDate}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default CreateList;
