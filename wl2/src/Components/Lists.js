8f887191baa5dd5f6ff26ab7e84d9de5490cec50
patrick-replogle <patrickr1138@gmail.com>
2019-10-19 11:01:25 -0700

    tweaked the format of the lists card a bit more.  Added delete and edit buttons too.

--- wl2/src/Components/Lists.js
+++ wl2/src/Components/Lists.js
@@ -4,8 +4,8 @@ import styled from "styled-components";
 const Card = styled.div`
   display: flex;
   flex-direction: column;
-  background: gray;
   width: 30%;
+  margin: 20px 20px;
 `;
 
 const TitleContainer = styled.div`
@@ -13,25 +13,69 @@ const TitleContainer = styled.div`
   margin-left: 5%;
 `;
 
+const H3 = styled.h3`
+  text-align: left;
+`;
+
 const TaskContainer = styled.div`
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: flex-end;
-  margin-right: 3%;
+  margin-left: 10%;
+`;
+
+const TaskPlusCheckbox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const DateContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: -25px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: flex-start;
+`;
+
+const Button = styled.button`
+  font-weight: bold;
+  background: black;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  margin-left: 10%;
+  padding: 5px 3px;
+  width: 20%;
+
+  &:hover {
+    background: white;
+    color: black;
+    border: 1px solid black;
+  }
 `;
 
 const Lists = props => {
   return (
     <Card>
-      <TitleContainer>
-        <h3>{props.list.title}</h3>
-      </TitleContainer>
+      <H3>{props.list.title}</H3>
+
       <TaskContainer>
-        <p>{props.list.task}</p>
-        <input type="checkbox" id="checkbox" name="checkbox" />
-        <p>{props.setDate}</p>
+        <TaskPlusCheckbox>
+          <p>{props.list.task}</p>
+          <input type="radio" id="checkbox" name="checkbox" />
+        </TaskPlusCheckbox>
+        <DateContainer>
+          <p>{props.list.setDate}</p>
+        </DateContainer>
       </TaskContainer>
-      <button onClick={() => props.deleteList(props.list.id)}>Delete</button>
+      <ButtonContainer>
+        <Button onClick={() => props.deleteList(props.list.id)}>Delete</Button>
+        <Button>Edit</Button>
+      </ButtonContainer>
     </Card>
   );
 };
