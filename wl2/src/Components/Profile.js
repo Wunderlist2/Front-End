1b3254abbc2d965fff44ebdeabd3d54e539155e7
Ethret <Ethret@users.noreply.github.com>
2019-10-19 10:21:52 -0500

    Just an initial commit of my profile setup

--- wl2/src/Components/Profile.js
+++ wl2/src/Components/Profile.js
@@ -1,11 +1,46 @@
 import React from "react";
+import Image from '../generic-user-icon.jpg';
 
-const Profile = () => {
-  return (
-    <div>
-      <h4>Profile</h4>
-    </div>
-  );
-};
+const Profile = (props) => {
+
+    return(
+        <div>
+          <div style={{display: "flex"}}>
+            <img src={Image} alt='test' style={{height: '100px', width: '100px'}}/>
+            <div>
+              <h2>Hello, Adric!</h2>
+              <h3>Sign Out</h3>
+            </div>
+          </div>
+          <br/>
+          <div style={{display: "flex"}}>
+            <img src={Image} alt='test' style={{height: '20px', width: '20px'}}/>
+            <p>Account</p>
+          </div>
+          <hr/>
+          <div style={{display: "flex"}}>
+            <img src={Image} alt='test' style={{height: '20px', width: '20px'}}/>
+            <p>Settings</p>
+          </div>
+          <hr/>
+          <div style={{display: "flex"}}>
+            <img src={Image} alt='test' style={{height: '20px', width: '20px'}}/>
+            <p>Notifications</p>
+          </div>
+          <hr/>
+          <div style={{display: "flex"}}>
+            <img src={Image} alt='test' style={{height: '20px', width: '20px'}}/>
+            <p>Sounds</p>
+          </div>
+          <hr/>
+          <div style={{display: "flex"}}>
+            <img src={Image} alt='test' style={{height: '20px', width: '20px'}}/>
+            <p>Email</p>
+          </div>
+        </div>
+    )
+}
 
 export default Profile;
+
+// <h2>Hello, {props.user.firstName}!</h2>
