0f12ab1ef1ed5fc17f8add04a4a59089d4a38f4c
Paul M Edwards <pauledwards@gmail.com>
2019-10-26 14:13:28 -0700

    Refactor all styles out of React and into LESS.

--- wl2/src/Components/Lists/ListItem.js
+++ wl2/src/Components/Lists/ListItem.js
@@ -9,211 +9,211 @@ import { faArrowLeft, faArrowRight, faPlus, faEdit, faTrashAlt } from "@fortawes
 
 class ListItem extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            user: {},
-            listID: '',
-            list: {},
-            tasks: [],
-            taskTitle: '',
-            setDate: '',
-            modal: false,
-            modalState: 'add',
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {},
+      listID: '',
+      list: {},
+      tasks: [],
+      taskTitle: '',
+      setDate: '',
+      modal: false,
+      modalState: 'add',
     }
+  }
 
-    componentDidMount() {
+  componentDidMount() {
 
+    axiosWithAuth()
+      .get(`/api/users/${localStorage.getItem('userID')}`)
+      .then(res => {
+        this.setState({
+          user: res.data,
+        })
+        this.getListID()
+      })
+      .then(() => {
         axiosWithAuth()
-            .get(`/api/users/${localStorage.getItem('userID')}`)
-            .then(res => {
-                this.setState({
-                    user: res.data,
-                })
-                this.getListID()
+          .get(`/api/todos/${this.state.listID}`)
+          .then(res => {
+            this.setState({
+              list: res.data,
             })
-            .then(() => {
-                axiosWithAuth()
-                    .get(`/api/todos/${this.state.listID}`)
-                    .then(res => {
-                        this.setState({
-                            list: res.data,
-                        })
-                        console.log('Get Request: ListItem.js: Result: ', res)
-                    })
-                    .catch(err => {
-                        console.log('Get Request: ListItem.js: Error: ', err)
-                    })
-            })
-            .catch(err => {
-                console.log('Get Request: Main: User Data: Error: ', err)
-            })
+            console.log('Get Request: ListItem.js: Result: ', res)
+          })
+          .catch(err => {
+            console.log('Get Request: ListItem.js: Error: ', err)
+          })
+      })
+      .catch(err => {
+        console.log('Get Request: Main: User Data: Error: ', err)
+      })
 
-    }
+  }
 
-    getListID = () => {
-        const urlParams = new URLSearchParams(window.location.search)
+  getListID = () => {
+    const urlParams = new URLSearchParams(window.location.search)
+    this.setState({
+      listID: urlParams.get('id')
+    })
+  }
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    })
+  }
+
+  deleteList = () => {
+    axiosWithAuth()
+      .delete(`/api/todos/${this.state.list.id}`)
+      .then(res => {
+        console.log('Delete Request: ListItem.js: Result: ', res)
+        this.props.history.push('/my')
+      })
+      .catch(err => {
+        console.log('Delete Request: ListItem.js: Error: ', err)
+      })
+  }
+
+  editList = () => {
+    axiosWithAuth()
+      .put(`/api/todos/${this.state.list.id}`, {
+        title: this.state.listTitle,
+        task: 'task',
+        setDate: 'today',
+      })
+      .then(res => {
+        console.log('Put Request: ListItem.js: Result: ', res)
         this.setState({
-            listID: urlParams.get('id')
+          lists: this.state.lists,
         })
+      })
+      .catch(err => {
+        console.log('Put Request: ListItem.js: Error: ', err)
+      })
+  }
+
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const obj = {
+      taskTitle: this.state.taskTitle,
+      setDate: this.state.setDate,
     }
+    this.state.tasks.push(obj)
+    this.setState({
+      tasks: this.state.tasks,
+    })
+    this.toggle()
+  }
 
-    toggle = () => {
-        this.setState({
-            modal: !this.state.modal,
-        })
-    }
+  handleNameSubmit = e => {
+    e.preventDefault()
+    this.editList()
+  }
 
-    deleteList = () => {
-        axiosWithAuth()
-            .delete(`/api/todos/${this.state.list.id}`)
-            .then(res => {
-                console.log('Delete Request: ListItem.js: Result: ', res)
-                this.props.history.push('/my')
-            })
-            .catch(err => {
-                console.log('Delete Request: ListItem.js: Error: ', err)
-            })
-    }
+  Tasks = () => {
+    return (
+      <div className="tasks">
+        <form
+          onSubmit={this.handleNameSubmit}
+          className="myTasks"
+        >
+          <input
+            id="listTitle"
+            label="Type to name the to-do"
+            name="listTitle"
+            placeholder={`${this.state.list.title}`}
+            className="taskTitleInput"
+            onChange={this.handleChanges}
+          />
+        </form>
 
-    editList = () => {
-        axiosWithAuth()
-            .put(`/api/todos/${this.state.list.id}`, {
-                title: this.state.listTitle,
-                task: 'task',
-                setDate: 'today',
-            })
-            .then(res => {
-                console.log('Put Request: ListItem.js: Result: ', res)
-                this.setState({
-                    lists: this.state.lists,
-                })
-            })
-            .catch(err => {
-                console.log('Put Request: ListItem.js: Error: ', err)
-            })
-    }
-
-    handleChanges = e => {
-        this.setState({
-            [e.target.name]: e.target.value,
-        })
-    }
-
-    handleSubmit = e => {
-        e.preventDefault()
-        const obj = {
-            taskTitle: this.state.taskTitle,
-            setDate: this.state.setDate,
-        }
-        this.state.tasks.push(obj)
-        this.setState({
-            tasks: this.state.tasks,
-        })
-        this.toggle()
-    }
-
-    handleNameSubmit = e => {
-        e.preventDefault()
-        this.editList()
-    }
-
-    Tasks = () => {
-        return (
-            <div style={{ margin: '0 32px' }}>
-                <form
-                    onSubmit={this.handleNameSubmit}
-                    style={{ fontSize: '32px', fontWeight: 600, textAlign: 'left' }}
-                >
-                    <input
-                        id="listTitle"
-                        label="Type to name the to-do"
-                        name="listTitle"
-                        placeholder={`${this.state.list.title}`}
-                        className="taskTitleInput"
-                        onChange={this.handleChanges}
-                    />
-                </form>
-
-                    {this.state.tasks.map(task => {
-                        return (
-                            <div key={task.id} style={{ width: '50%', margin: '0 64px 32px', }}>
-                                <div  style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                                    <div style={{ display: 'block', marginRight: '16px', textAlign: 'left' }}>
-                                        <div style={{ marginBottom: '8px', fontSize: '24px', fontWeight: 600 }}>{task.taskTitle}</div>
-                                        <div style={{ fontSize: '16px', fontWeight: 400, color: '#757575' }}>{task.setDate}</div>
-                                    </div>
-                                    <FontAwesomeIcon icon={faArrowRight} style={{ fontSize: '24px' }} />
-                                </div>
-                            </div>
-                        );
-                    })}
-
+        {this.state.tasks.map(task => {
+          return (
+            <div key={task.id} className="taskEntryContainer">
+              <div className="taskEntryWithArrow">
+                <div className="taskEntry">
+                  <div className="taskEntryTitle">{task.taskTitle}</div>
+                  <div className="taskEntryDate">{task.setDate}</div>
+                </div>
+                <FontAwesomeIcon icon={faArrowRight} className="faIcon" />
+              </div>
             </div>
-        );
-    }
+          );
+        })}
 
-    Modal = () => {
-        return (
+      </div>
+    );
+  }
+
+  Modal = () => {
+    return (
+      <div>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className="centeredModal">
+          <div className="modalDiv">
             <div>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.54)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <div className="createListModal">
-                        <div>
-                            <input
-                                id="listTitle"
-                                label="Type to name the to-do"
-                                name="taskTitle"
-                                placeholder="Type to create a to-do"
-                                className="listTitleInput"
-                                autoFocus
-                                onChange={this.handleChanges}
-                            />
-                            <input
-                                type="date"
-                                id="form-input"
-                                name="setDate"
-                                className="listTitleInput"
-                                onChange={this.handleChanges}
-                            />
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
-                            <button className="cancelNewListButton" onClick={this.toggle}>Cancel</button>
-                            <button className="createNewListButton" onClick={this.handleSubmit}>Create</button>{' '}
-                        </div>
-                    </div>
-                </Modal>
+              <input
+                id="listTitle"
+                label="Type to name the to-do"
+                name="taskTitle"
+                placeholder="Type to create a to-do"
+                className="modalInput"
+                autoFocus
+                onChange={this.handleChanges}
+              />
+              <input
+                type="date"
+                id="form-input"
+                name="setDate"
+                className="modalInput"
+                onChange={this.handleChanges}
+              />
             </div>
-        );
-    }
+            <div className="modalButtonDiv">
+              <button className="cancelNewListButton" onClick={this.toggle}>Cancel</button>
+              <button className="createNewListButton" onClick={this.handleSubmit}>Create</button>{' '}
+            </div>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <>
-                <header className="listItemHeader">
-                    <div className="headerCtn">
-                        <div className="headerLeftIconCtn">
-                            <NavLink to="/my">
-                                <FontAwesomeIcon icon={faArrowLeft} style={{ fontSize: '24px' }} />
-                            </NavLink>
-                        </div>
-                        <div onClick={this.deleteList} className="headerRightIconCtn">
-                            <FontAwesomeIcon icon={faTrashAlt} style={{ fontSize: '24px', marginLeft: '24px' }} />
-                        </div>
-                    </div>
-                </header>
-                <this.Tasks />
-                <this.Modal />
-                <footer>
-                    <div className="footerListItem">
-                        <div onClick={this.toggle}>
-                            <FontAwesomeIcon icon={faPlus} className="button createButton" />
-                        </div>
-                    </div>
-                </footer>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <header className="listItemHeader">
+          <div className="headerCtn">
+            <div className="headerLeftIconCtn">
+              <NavLink to="/my">
+                <FontAwesomeIcon icon={faArrowLeft} className="faIcon" />
+              </NavLink>
+            </div>
+            <div onClick={this.deleteList} className="headerRightIconCtn">
+              <FontAwesomeIcon icon={faTrashAlt} className="faIcon" />
+            </div>
+          </div>
+        </header>
+        <this.Tasks />
+        <this.Modal />
+        <footer>
+          <div className="footerListItem">
+            <div onClick={this.toggle}>
+              <FontAwesomeIcon icon={faPlus} className="button createButton" />
+            </div>
+          </div>
+        </footer>
+      </>
+    );
+  }
 
 }
 
--- wl2/src/Components/MainPage.js
+++ wl2/src/Components/MainPage.js
@@ -6,168 +6,168 @@ import { Link, NavLink } from "react-router-dom";
 import { Modal } from 'reactstrap';
 
 import defaultPhoto from '../generic-user-icon.jpg';
-import {faArrowRight, faPlus, faSearch} from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class MainPage extends React.Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            user: {},
-            lists: [],
-            modal: false,
-            listTitle: '',
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {},
+      lists: [],
+      modal: false,
+      listTitle: '',
     }
+  }
 
-    componentDidMount() {
+  componentDidMount() {
 
-        axiosWithAuth()
-            .get(`/api/users/${localStorage.getItem('userID')}`)
-            .then(res => {
-                this.setState({
-                    user: res.data,
-                })
-                console.log(res.data)
-            })
-            .catch(err => {
-                console.log('Get Request: Main: User Data: Error: ', err)
-            })
-
-        axiosWithAuth()
-            .get(`/api/todos/`)
-            .then(res => {
-                this.setState({
-                    lists: res.data.reverse(),
-                })
-            })
-            .catch(err => {
-                console.log('Get Request: Main: Todo List: Error: ', err)
-            })
-
-    }
-
-    toggle = () => {
+    axiosWithAuth()
+      .get(`/api/users/${localStorage.getItem('userID')}`)
+      .then(res => {
         this.setState({
-            modal: !this.state.modal,
+          user: res.data,
         })
-    }
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log('Get Request: Main: User Data: Error: ', err)
+      })
 
-    handleChanges = e => {
+    axiosWithAuth()
+      .get(`/api/todos/`)
+      .then(res => {
         this.setState({
-            listTitle: e.target.value,
+          lists: res.data.reverse(),
         })
-    }
+      })
+      .catch(err => {
+        console.log('Get Request: Main: Todo List: Error: ', err)
+      })
 
-    handleSubmit = e => {
-        e.preventDefault()
-        axiosWithAuth()
-            .post('/api/todos/', {
-                title: this.state.listTitle,
-                task: 'task',
-                setDate: 'today',
-            })
-            .then(res => {
-                console.log('Post Request: MainPage: Result: ', res.data)
-                this.state.lists.unshift(res.data)
-                this.setState({
-                    lists: this.state.lists,
-                    modal: !this.state.modal,
-                })
-            })
-            .catch(err => console.log('Post Request: MainPage: Error: ', err))
-    }
+  }
 
-    Lists = () => {
-        if (this.state.lists.length >= 1) {
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal,
+    })
+  }
+
+  handleChanges = e => {
+    this.setState({
+      listTitle: e.target.value,
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    axiosWithAuth()
+      .post('/api/todos/', {
+        title: this.state.listTitle,
+        task: 'task',
+        setDate: 'today',
+      })
+      .then(res => {
+        console.log('Post Request: MainPage: Result: ', res.data)
+        this.state.lists.unshift(res.data)
+        this.setState({
+          lists: this.state.lists,
+          modal: !this.state.modal,
+        })
+      })
+      .catch(err => console.log('Post Request: MainPage: Error: ', err))
+  }
+
+  Lists = () => {
+    if (this.state.lists.length >= 1) {
+      return (
+        <div className="lists">
+          <div className="myLists">My Lists</div>
+          {this.state.lists.map(list => {
             return (
-                <div style={{ margin: '0 32px' }}>
-                    <div style={{ fontSize: '32px', fontWeight: 600, marginBottom: '56px', textAlign: 'left' }}>My Lists</div>
-                    {this.state.lists.map(list => {
-                        return (
-                            <div key={list.id} style={{ width: '50%', margin: '0 64px 32px', }}>
-                                <Link to={`/my/list?id=${list.id}`} >
-                                    <div  style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                                        <div style={{ display: 'block', marginRight: '16px', textAlign: 'left' }}>
-                                            <div style={{ marginBottom: '8px', fontSize: '24px', fontWeight: 600 }}>{list.title}</div>
-                                            <div style={{ fontSize: '16px', fontWeight: 400, color: '#757575' }}>task count</div>
-                                        </div>
-                                        <FontAwesomeIcon icon={faArrowRight} style={{ fontSize: '24px' }} />
-                                    </div>
-                                </Link>
-                            </div>
-                        );
-                    })}
-                </div>
+              <div key={list.id} className="listEntryContainer">
+                <Link to={`/my/list?id=${list.id}`}>
+                  <div className="listEntryWithArrow">
+                    <div className="listEntry">
+                      <div className="listEntryTitle">{list.title}</div>
+                      <div className="listEntryCount">task count</div>
+                    </div>
+                    <FontAwesomeIcon icon={faArrowRight} className="faIcon"/>
+                  </div>
+                </Link>
+              </div>
             );
-        }
-        else {
-            return (
-                <div className="noData" style={{ width: '90%', margin: '0 auto 112px', }}>
-                    <div style={{ fontSize: '32px', fontWeight: 600, marginBottom: '56px', textAlign: 'left' }}>Welcome to Wunderlist!</div>
-                    <div style={{ fontSize: '24px', fontWeight: 400, color: '#757575', textAlign: 'left' }}>Click the plus button below to add your first list.</div>
-                </div>
-            );
-        }
+          })}
+        </div>
+      );
     }
+    else {
+      return (
+        <div className="noData">
+          <div className="welcomeDiv">Welcome to Wunderlist!</div>
+          <div className="hintDiv">Click the plus button below to add your first list.</div>
+        </div>
+      );
+    }
+  }
 
-    Modal = () => {
-        return (
+  Modal = () => {
+    return (
+      <div>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className="centeredModal">
+          <div className="modalDiv">
             <div>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.54)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <div className="createListModal">
-                        <div>
-                            <input
-                                id="listTitle"
-                                label="Type to name your list"
-                                name="listTitle"
-                                placeholder="Type to name your list"
-                                className="listTitleInput"
-                                autoFocus
-                                onChange={this.handleChanges}
-                            />
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
-                            <button className="cancelNewListButton" onClick={this.toggle}>Cancel</button>
-                            <button className="createNewListButton" onClick={this.handleSubmit}>Create</button>{' '}
-                        </div>
-                    </div>
-                </Modal>
+              <input
+                id="listTitle"
+                label="Type to name your list"
+                name="listTitle"
+                placeholder="Type to name your list"
+                className="modalInput"
+                autoFocus
+                onChange={this.handleChanges}
+              />
             </div>
-        );
-    }
+            <div className="modalButtonDiv">
+              <button className="cancelNewListButton" onClick={this.toggle}>Cancel</button>
+              <button className="createNewListButton" onClick={this.handleSubmit}>Create</button>{' '}
+            </div>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <UserContext.Provider user={this.state.user}>
-                <header>
-                    <div className="title">
-                        <span className="titleMessage">
-                            {`Hello,  ${this.state.user.first_name}!`}
-                        </span>
-                        <span className="userPhoto">
-                            <NavLink to="/my/profile">
-                                <img src={defaultPhoto} alt="User" />
-                            </NavLink>
-                        </span>
-                    </div>
-                </header>
-                <this.Modal />
-                <this.Lists />
-                <footer>
-                    <div className="footerItems">
-                        <div>
-                            <FontAwesomeIcon icon={faSearch} className="button searchButton" />
-                        </div>
-                        <div onClick={this.toggle}>
-                            <FontAwesomeIcon icon={faPlus} className="button createButton" />
-                        </div>
-                    </div>
-                </footer>
-            </UserContext.Provider>
-        );
-    }
+  render() {
+    return (
+      <UserContext.Provider user={this.state.user}>
+        <header>
+          <div className="title">
+            <span className="titleMessage">
+              {`Hello,  ${this.state.user.first_name}!`}
+            </span>
+            <span className="userPhoto">
+              <NavLink to="/my/profile">
+                <img src={defaultPhoto} alt="User" />
+              </NavLink>
+            </span>
+          </div>
+        </header>
+        <this.Modal />
+        <this.Lists />
+        <footer>
+          <div className="footerItems">
+            <div>
+              <FontAwesomeIcon icon={faSearch} className="button searchButton" />
+            </div>
+            <div onClick={this.toggle}>
+              <FontAwesomeIcon icon={faPlus} className="button createButton" />
+            </div>
+          </div>
+        </footer>
+      </UserContext.Provider>
+    );
+  }
 
 }
 
